e2a4e91dca13fbb659fc98710b8663f7999a5534
hzcethan <3111267307@qq.com>
2023-11-03 00:51:59 +0800

    submit2

--- vue.config.js
+++ vue.config.js
@@ -3,26 +3,22 @@ const { defineConfig } = require('@vue/cli-service')
 //   transpileDependencies: true
 // })
 
-
-module.exports = {
-  devServer:{
-    open:true,
-    proxy:{// http-proxy-middware
-      '/baidu':{
-        target:'http://www.baidu.com'
+module.exports = defineConfig({
+  devServer: {
+    open: true,
+    // http-proxy-middware
+    proxy: {
+      '/baidu': {
+        target: 'http://www.baidu.com'
       },
-      // 应用内的ajax访问到了/api 则算命中,按照当前规则做代理
-      '/jindu':{
-        target:'http://jinduedu.net:5004',
-        pathRewrite:{
-          '^/jindu':'/api'
-        },
-        logLevel:'debug', // 以调试级别来输出日志
-        // "@vue/cli-service": "~5.0.0", 4~5的版本需要加上如下一行来显示日志
-        logProvider:()=>console,
-        // 让最终的接口服务器看到真实客户端的IP，而非代理IP
-        changeOrigin:true
+      // 应用内的Ajax访问到了/jindu 则算命中，按照当前规则做代理
+      '/jindu': {
+        target: 'http://jinduedu.net:5004',
+        // 用于重写 URL 路径。它允许你将特定的路径映射到不同的路径
+        pathRewrite: {
+          '^/jindu': '/api'
+        }
       }
     }
   }
-}
+})
