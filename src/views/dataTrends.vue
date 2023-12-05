715a49c5f11929664a21e07baa9ebdaba44e7a46
hzcethan <3111267307@qq.com>
2023-12-06 01:13:41 +0800

    submit

---
+++ src/views/dataTrends.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <iframe src="http://localhost:5173/" class="cale-h" width="100%" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.cale-h {
+  //   height: calc(100vh - 152px);
+  height: 100vh;
+}
+</style>
